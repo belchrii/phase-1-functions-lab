51fcaa9ec681fdc35a065ffb43b24cece81f8572
chris bell <elmntwzard@gmail.com>
2023-04-07 16:51:09 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(somevalue) {
+   const headquarters = 42
+   return Math.abs(somevalue - headquarters);
+}
+
+function distanceFromHqInFeet(somevalue){
+    return distanceFromHqInBlocks(somevalue) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    let feet = Math.abs(destination-start) ;
+return feet*264;
+    }
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0;
+    } else if (distance >= 400 && distance <= 2000){
+        return Math.abs(distance - 400)*0.02;
+    } else if (distance >= 2000 && distance <= 2500){
+        return 25;
+    } else if (distance > 2500){
+        return 'cannot travel that far'
+    }
+}   
